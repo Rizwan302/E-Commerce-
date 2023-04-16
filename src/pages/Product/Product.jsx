7f6e6dbe4d0b0252c50052a48b27c1b68819b660
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-04-17 02:01:15 +0500

    Set Product components

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import './Product.scss'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import BalanceIcon from '@mui/icons-material/Balance';
 
 export default function Product() {
 
   const [selectedImg, setselectedImg] = useState(0)  
+  const [quantity, setquantity] = useState(1)  
 
   const images = [
     "https://images.pexels.com/photos/14592813/pexels-photo-14592813.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
@@ -22,7 +26,42 @@ export default function Product() {
       <div className="mainImg">
         <img src={images[selectedImg]} alt="" />
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className='price'>$199</span>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam sunt deserunt tenetur architecto molestiae obcaecati quia repellat, totam saepe corrupti quod rerum voluptas, amet voluptates ipsa dolore pariatur nesciunt! Et!</p>
+        <div className="quantity">
+          <button onClick={()=> setquantity((prev) => prev===1 ? 1: prev-1)}>-</button>
+          {quantity}
+          <button onClick={()=> setquantity(prev => prev+1)}>+</button>
+        </div>
+        <button className='add'>
+            <AddShoppingCartIcon/> ADD TO CART
+        </button>
+        <div className="link">
+          <div className="item">
+            <FavoriteBorderIcon/>ADD TO WISH LIST
+          </div>
+          <div className="item">
+            <BalanceIcon/> ADD TO COMPARE
+          </div>
+        </div>
+
+        <div className="info">
+          <span>Vendor: polo</span>
+          <span>Product Type T-Shirt</span>
+          <span>Tag: T_Shirt, Women, Top</span>
+        </div>
+        <hr/>
+
+        <div className="details">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATTION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
     </div>
   )
 }
